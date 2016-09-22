bc1f6b58a6f0e26cf85bdd2e4372fda7cad454bf
shaneehlert <shane@esperas.org>
2016-09-22 14:42:26 -0700

    updated jobs

--- controllers/job.js
+++ controllers/job.js
@@ -1,5 +1,5 @@
 var express = require('express');
-var Recipe = require('../models/recipe');
+var Job = require('../models/Job');
 var router = express.Router();
 
 router.route('/')
@@ -11,30 +11,30 @@ router.route('/')
     });
   })
   .post(function(req, res) {
-    Recipe.create(req.body, function(err, recipe) {
+    Job.create(req.body, function(err, recipe) {
       if (err) return res.status(500).send(err);
 
-      return res.send(recipe);
+      return res.send(job);
     });
   });
 
 router.route('/:id')
   .get(function(req, res) {
-    Recipe.findById(req.params.id, function(err, recipe) {
+    Job.findById(req.params.id, function(err, recipe) {
       if (err) return res.status(500).send(err);
 
-      return res.send(recipe);
+      return res.send(job);
     });
   })
   .put(function(req, res) {
-    Recipe.findByIdAndUpdate(req.params.id, req.body, function(err) {
+    Job.findByIdAndUpdate(req.params.id, req.body, function(err) {
       if (err) return res.status(500).send(err);
 
       return res.send({ message: 'success' });
     });
   })
   .delete(function(req, res) {
-    Recipe.findByIdAndRemove(req.params.id, function(err) {
+    Job.findByIdAndRemove(req.params.id, function(err) {
       if (err) return res.status(500).send(err);
 
       return res.send({ message: 'success' });
--- public/app/app.js
+++ public/app/app.js
@@ -1,4 +1,4 @@
-var app = angular.module('RecipeApp', ['ui.router', 'RecipeCtrls']);
+var app = angular.module('JobApp', ['ui.router', 'JobCtrls']);
 
 app.config([
   '$stateProvider',
@@ -13,17 +13,17 @@ app.config([
     $stateProvider
   .state('home', {
     url: '/',
-    templateUrl: 'app/views/recipes.html',
+    templateUrl: 'app/views/job.html',
     controller: 'HomeCtrl'
   })
-  .state('newRecipe', {
-    url: '/recipes/new',
-    templateUrl: 'app/views/newRecipe.html',
+  .state('newJob', {
+    url: '/job/new',
+    templateUrl: 'app/views/newJob.html',
     controller: 'NewCtrl'
   })
-  .state('recipeShow', {
-    url: '/recipes/:id',
-    templateUrl: 'app/views/showRecipe.html',
+  .state('recipeJob', {
+    url: '/job/:id',
+    templateUrl: 'app/views/showJob.html',
     controller: 'ShowCtrl'
   })
   .state('signup', {
