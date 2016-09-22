e4bc8594cfe000a5eef88378ea83c506b195f9bc
ChrisKenyon79 <ckenyon79@gmail.com>
2016-09-22 11:52:08 -0700

    recipes copy

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# jobhunt
+#jobhunt
+d
---
+++ public/app/controllers.js
@@ -0,0 +1,76 @@
+angular.module('RecipeCtrls', ['RecipeServices'])
+.controller('HomeCtrl', ['$scope', 'Recipe', function($scope, Recipe) {
+  $scope.recipes = [];
+
+  Recipe.query(function success(data) {
+    $scope.recipes = data;
+  }, function error(data) {
+    console.log(data);
+  });
+
+  $scope.deleteRecipe = function(id, recipesIdx) {
+    Recipe.delete({ id: id }, function success(data) {
+      $scope.recipes.splice(recipesIdx, 1);
+    }, function error(data) {
+      console.log(data);
+    });
+  };
+}])
+.controller('ShowCtrl', ['$scope', '$stateParams', 'Recipe', function($scope, $stateParams, Recipe) {
+  $scope.recipe = {};
+
+  Recipe.get({ id: $stateParams.id }, function success(data) {
+    $scope.recipe = data;
+  }, function error(data) {
+    console.log(data);
+  });
+}])
+.controller('NewCtrl', ['$scope', '$location', 'Recipe', function($scope, $location, Recipe) {
+  $scope.recipe = {
+    title: '',
+    description: '',
+    image: ''
+  };
+
+  $scope.createRecipe = function() {
+    Recipe.save($scope.recipe, function success(data) {
+      $location.path('/');
+    }, function error(data) {
+      console.log(data);
+    });
+  };
+}])
+.controller('NavCtrl', ['$scope', 'Auth', function($scope, Auth) {
+  $scope.Auth = Auth;
+  $scope.logout = function() {
+    Auth.removeToken();
+    console.log('My Token:', Auth.getToken());
+  };
+}])
+.controller('SignupCtrl', ['$scope', '$http', '$state', function($scope, $http, $state) {
+  $scope.user = {
+    email: '',
+    password: ''
+  };
+  $scope.userSignup = function() {
+    $http.post('/api/users', $scope.user).then(function success(res) {
+      $state.go('home');
+    }, function error(res) {
+      console.log(res);
+    });
+  };
+}])
+.controller('LoginCtrl', ['$scope', '$http', '$state', 'Auth', function($scope, $http, $state, Auth) {
+  $scope.user = {
+    email: '',
+    password: ''
+  };
+  $scope.userLogin = function() {
+    $http.post('/api/auth', $scope.user).then(function success(res) {
+      Auth.saveToken(res.data.token);
+      $state.go('home');
+    }, function error(res) {
+      console.log(res);
+    });
+  };
+}]);
